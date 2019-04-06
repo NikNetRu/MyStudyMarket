217c3f14309fdf4c5747d96add2480755eac3014
NikNetRu <NikNetRu@list.ru>
2019-04-06 20:34:58 +0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>MiniMarket</title>
+        <link href="CSS/styles.css" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    </head>
+    <body>
+     <!-- Тут только про хеадер-->    
+     <div id ="myhead">
+         <header class="bigText">
+             <h1>MiniMarket</h1>
+         </header> 
+     </div>
+     
+     
+        <!--Левая панель--> 
+        <div name ="leftblock" id = "leftblock" class="smallText">
+            <label id="Catalog"> Каталог </label> <br>
+            <label id="BuyAndPost"> Оплата и доставка </label> <br>
+            <label id ="Contacts"> Контакты </label> <br>
+        </div>
+        
+        <!--Центральный блок-->
+    <div name ="centralblock" id = "centralblock"></div>
+        <!--Правая панель-->
+        <div name ="rightblock" id = "rightblock">
+            <label>Логин</label><br><br><br>
+            <input id="email" placeholder="E-mail"> <br> <br><br>
+            <label>Пароль</label> <br><br><br>
+            <input id="password" placeholder="Password"> <br>
+            
+        <!--Бутер-->
+    <div id ="myboot" class="bigText">
+             <h2>HiHo</h2>
+     </div>
+        <?php
+        
+        ?>
+    </body>
+</html>
